278ca3171ed8a3a5bc5baf130c49f94a08b173de
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2025-09-10 19:47:08 +0530

    create frontend vite

---
+++ frontend/src/pages/userLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const userLogin = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">User Login</h1>
+    </div>
+  )
+}
+
+export default userLogin
